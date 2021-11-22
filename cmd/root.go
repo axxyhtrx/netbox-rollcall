89dc57b57d6d1b47cbaf155518561c04c3476427
Anatoly Kolpakov <anatoly.kolpakov@nokia.com>
2021-11-22 15:37:57 +0300

    config not found

--- cmd/root.go
+++ cmd/root.go
@@ -60,5 +60,8 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Fprintln(os.Stderr, "Please create configuration file config.yaml in a root of the project")
 	}
+
 }
