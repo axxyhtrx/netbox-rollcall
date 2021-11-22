6bf27a0ff8e272c2a4d03cd4989b0af2d786c33b
Anatoly Kolpakov <anatoly.kolpakov@nokia.com>
2021-11-22 14:41:15 +0300

    cleanup

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -27,8 +12,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "rollcall",
-	Short: "A brief description of your application",
-	Long:  `.`,
+	Short: "Network scanner for Netbox",
+	Long:  `Network scanner for Netbox, based on nmap`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -75,12 +60,5 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-	var debug = false
-	if debug {
-		fmt.Println(viper.AllSettings())
-		fmt.Println(viper.Get("config"))
-		fmt.Println(viper.GetString("netbox.netboxhost"))
-		fmt.Println(viper.GetString("netbox.netboxapiurl"))
 	}
 }
